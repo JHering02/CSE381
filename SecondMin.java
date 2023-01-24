84bb4cde0d2f2ced744258aea0ee6dc920b6b992
James <heringja@miamioh.edu>
2023-01-24 11:39:55 -0500

    Made a thing

--- SecondMin.java
+++ SecondMin.java
@@ -1,3 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+public class SecondMin {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        Scanner reader = new Scanner("input.txt");
+        while(reader.hasNextInt()) {
+
+        }
+    }
+}
